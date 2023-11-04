66f8f235d80bd9bc6dd16bfcf8d544965d25159d
sourav <sourav.dubey@kreatetechnologies.com>
2023-11-05 01:58:45 +0530

    test venv

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 
 from flask import Flask,render_template
-from flask_socketio import SocketIO, emit
+# from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+# socketio = SocketIO(app)
 
 @app.route('/')
 def hello_world():
     return render_template('welcome.html')
 
-@socketio.on('message')
-def handle_message(data):
-    message = data['message']
-    emit('message', {'message': message}, broadcast=True)
+# @socketio.on('message')
+# def handle_message(data):
+#     message = data['message']
+#     emit('message', {'message': message}, broadcast=True)
 
-@app.route('/chat')
-def index():
-    return render_template('index.html')
+# @app.route('/chat')
+# def index():
+#     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    socketio.run(app)
+    # socketio.run(app)
+    app.run()
