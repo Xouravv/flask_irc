8239353d80068101ea58eab97df7afd376241098
sourav <sourav.dubey@kreatetechnologies.com>
2023-11-05 16:28:19 +0530

    db test login page take 108

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{username}:{password}@{hostname}/{dat
 
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 db = SQLAlchemy(app)
-db.create_all()
+with app.app_context(): 
+    db.create_all()
 
 from models import cruduser
 
