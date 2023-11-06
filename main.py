8b4dc36dd4aaff789b0fcf8a193015e58a605d6e
sourav <sourav.dubey@kreatetechnologies.com>
2023-11-06 18:34:52 +0530

    wsgi socekt issue

--- main.py
+++ main.py
@@ -133,5 +133,10 @@ def chat(username,room):
     
 
 if __name__ == '__main__':
-    socketio.run(app)
+
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+
+    server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+    server.serve_forever()
     # app.run()
